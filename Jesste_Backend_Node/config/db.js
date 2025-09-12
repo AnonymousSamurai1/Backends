8296413c128fa695381a51cc2a8ed46a53ee89e2
AnonymousSamurai1 <anonymoussamuraiedem@gmail.com>
2025-09-12 22:03:10 +0000

    Delete Jesste_Backend_Node directory

--- Jesste_Backend_Node/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const connectDB = async()=>{
-   const conn = await mongoose.connect("mongodb+srv://anonymous:Jesus123@techedem.ol5kcgf.mongodb.net/")
-   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
-}
- module.exports=connectDB
